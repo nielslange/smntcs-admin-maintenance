afd71a015bb26985108b47bb4aa405cabe77febf
Niels Lange <info@nielslange.de>
2022-01-09 11:20:22 +0700

    Update e2e tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,51 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add( 'loginAsAdmin', () => {
+	cy.visit( 'http://localhost:8888/wp-login.php' ).wait( 100 );
+	cy.get( '#user_login' ).type( 'admin' );
+	cy.get( '#user_pass' ).type( 'password' );
+	cy.get( '#wp-submit' ).click();
+} );
+
+Cypress.Commands.add( 'loginAsEditor', () => {
+	cy.visit( 'http://localhost:8888/wp-login.php' ).wait( 100 );
+	cy.get( '#user_login' ).type( 'editor' );
+	cy.get( '#user_pass' ).type( 'editor' );
+	cy.get( '#wp-submit' ).click();
+} );
+
+Cypress.Commands.add( 'createEditorAccount', () => {
+	cy.visit( 'http://localhost:8888/wp-admin/user-new.php' ).wait( 100 );
+	cy.get( "input[id='user_login']" ).type( 'editor' );
+	cy.get( "input[id='email']" ).type( 'editor@example.com' );
+	cy.get( "input[id='pass1']" ).clear().type( 'editor' );
+	cy.get( "input[class='pw-checkbox']" ).check();
+	cy.get( 'input[id="createusersub"]' ).click();
+} );
+
+Cypress.Commands.add( 'deleteEditorAccount', () => {
+	cy.visit( 'http://localhost:8888/wp-admin/users.php' ).wait( 100 );
+	cy.get( "input[id='user-search-input']" ).type( 'editor' );
+	cy.get( "input[id='search-submit']" ).click();
+	cy.get( "input[id='cb-select-all-1']" ).click();
+	cy.get( "select[id='bulk-action-selector-top']" ).select( 'delete' );
+	cy.get( '#doaction' ).click();
+	cy.get( '#submit' ).click();
+} );
+
+Cypress.Commands.add( 'activatePlugin', () => {
+	cy.visit( 'http://localhost:8888/wp-admin/customize.php' ).wait( 100 );
+	cy.get( '#accordion-section-smntcs_admin_maintenance_section' )
+		.click()
+		.wait( 500 );
+	cy.get( '#_customize-input-smntcs_admin_maintenance_enable' ).check();
+	cy.get( '#_customize-input-smntcs_admin_maintenance_uid' ).select( '1' );
+	cy.get( '#save' ).click();
+} );
+
+Cypress.Commands.add( 'deactivatePlugin', () => {
+	cy.visit( 'http://localhost:8888/wp-admin/customize.php' ).wait( 100 );
+	cy.get( '#accordion-section-smntcs_admin_maintenance_section' )
+		.click()
+		.wait( 500 );
+	cy.get( '#_customize-input-smntcs_admin_maintenance_enable' ).uncheck();
+	cy.get( '#save' ).click();
+} );
